d9947c6a527a2a346b52b2e49299bb73581c2356
Juan Carlos Avila <juanvs23@gmail.com>
2020-03-28 21:39:07 -0400

    primer diseño

---
+++ coltmanpruebaapi.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * The plugin bootstrap file
+ *
+ * This file is read by WordPress to generate the plugin information in the plugin
+ * admin area. This file also includes all of the dependencies used by the plugin,
+ * registers the activation and deactivation functions, and defines a function
+ * that starts the plugin.
+ *
+ * @link              https://www.google.com
+ * @since             1.0.0
+ * @package           coltmanpruebaapi
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Coltman PruebaApi
+ * Plugin URI:        https://www.google.com
+ * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
+ * Version:           1.0.0
+ * Author:            Juan Carlos Avila Pérez
+ * Author URI:        https://www.google.com
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       coltmanpruebaapi
+ * Domain Path:       /languages
+ */
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+};
+
+?>
